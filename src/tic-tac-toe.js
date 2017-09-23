da295f5be3f81dc49604933d8a8ffd1501565d5e
asb1122 <asb1122@yandex.ru>
2017-09-23 15:16:33 +0300

    solution

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,74 @@
 class TicTacToe {
-    constructor() {
 
+  constructor(currentPlayerSymbol) {
+    this.currentPlayerSymbol = 'x';
+    this.matrix = [[null, null ,null],[null, null ,null],[null, null ,null]];
+    this.turns = 0;
+    this.winner = null;
+    this.isWinner = false;
+  }
+
+  getCurrentPlayerSymbol() {
+    return this.currentPlayerSymbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (this.matrix[rowIndex][columnIndex] == null){
+    this.matrix[rowIndex][columnIndex] = this.currentPlayerSymbol;
+    if (this.currentPlayerSymbol == 'o'){
+      this.currentPlayerSymbol = 'x';
+    } else if (this.currentPlayerSymbol == 'x'){
+      this.currentPlayerSymbol = 'o';
     }
-
-    getCurrentPlayerSymbol() {
-
+    this.turns++;
+    this.isWinner = (this.matrix[0][0] == this.matrix[0][1] && this.matrix[0][0] == this.matrix[0][2] && this.matrix[0][0] != null)
+    || (this.matrix[1][0] == this.matrix[1][1] && this.matrix[1][0] == this.matrix[1][2] && this.matrix[1][0] != null)
+    || (this.matrix[2][0] == this.matrix[2][1] && this.matrix[2][0] == this.matrix[2][2] && this.matrix[2][0] != null )
+    || (this.matrix[0][0] == this.matrix[1][0] && this.matrix[0][0] == this.matrix[2][0] && this.matrix[0][0] != null )
+    || (this.matrix[0][1] == this.matrix[1][1] && this.matrix[0][1] == this.matrix[2][1] && this.matrix[0][1] != null)
+    || (this.matrix[0][2] == this.matrix[1][2] && this.matrix[0][2] == this.matrix[2][2] && this.matrix[0][2] != null)
+    || (this.matrix[0][0] == this.matrix[1][1] && this.matrix[0][0] == this.matrix[2][2] && this.matrix[0][0] != null)
+    || (this.matrix[0][2] == this.matrix[1][1] && this.matrix[0][2] == this.matrix[2][0] && this.matrix[0][2] != null);
     }
+  }
 
-    nextTurn(rowIndex, columnIndex) {
+  isFinished() {
+    return this.isWinner == true || this.noMoreTurns();
+  }
 
+  getWinner() {
+    if (this.isWinner == true){
+      if (this.currentPlayerSymbol == 'x'){
+        return 'o';
+      } else {
+        return 'x';
+      }
+    } else {
+      return null;
     }
+  }
 
-    isFinished() {
-
+  noMoreTurns() {
+    if (this.turns >= 9){
+      return true;
+    } else {
+      return false;
     }
+  }
 
-    getWinner() {
-
+  isDraw() {
+    if (this.isWinner == true || this.noMoreTurns() == false ||
+        (this.isWinner == true && this.noMoreTurns() ==  true)){
+      return false;
+    } else {
+      return true;
     }
+  }
 
-    noMoreTurns() {
+  getFieldValue(rowIndex, colIndex) {
+    return this.matrix[rowIndex][colIndex];
+  }
 
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
 }
 
 module.exports = TicTacToe;
